b9473b480b85466733dca5e77739b90614be4e0f
살려주세요 <46598063+qwreey75@users.noreply.github.com>
2021-08-21 18:03:17 +0900

    init commit

---
+++ futils.lua
@@ -0,0 +1,17 @@
+--[[
+module name :: File Utils
+this module just simply scan folder; and returns data that includes scaned files, folders
+
+returnData = {
+    {
+        isFolder = false;
+        name = "someFolder";
+    };
+    {
+        isFolder = true;
+        name = "someFolder";
+    };
+}
+]]
+
+function scan
---
+++ server/init.lua
@@ -0,0 +1,56 @@
+local http = require('http');
+local url = require('url');
+local fs = require('fs');
+local Response = require('http').ServerResponse;
+local mimes = require('server.mimes');
+local root = "docs";
+
+-- Return 404 error (file was not found)
+function Response:notFound(reason)
+    self:writeHead(404, {
+        ["Content-Type"] = "text/plain";
+        ["Content-Length"] = #reason;
+    })
+    self:write(reason);
+end
+
+-- Return 500 error
+function Response:error(reason)
+    self:writeHead(500, {
+        ["Content-Type"] = "text/plain";
+        ["Content-Length"] = #reason;
+    })
+    self:write(reason);
+end
+
+http.createServer(function(req, res)
+    req.uri = url.parse(req.url);
+    local path do
+        local pathName = req.uri.pathname;
+        if pathName == "/" then
+            pathName = "/index.html";
+        end
+        path = root .. pathName;
+    end
+    io.write("request file: ",path,"\n");
+    fs.stat(path, function (err, stat)
+        if err then
+            if err.code == "ENOENT" then
+                return res:notFound(err.message .. "\n");
+            end
+            return res:error((err.message or tostring(err)) .. "\n");
+        end
+        if stat.type ~= 'file' then
+            return res:notFound("Requested url is not a file\n");
+        end
+
+        res:writeHead(200, {
+            ["Content-Type"] = mimes.getType(path),
+            ["Content-Length"] = stat.size
+        });
+
+        fs.createReadStream(path):pipe(res);
+    end);
+end):listen(8080);
+
+io.write "Http static file server listening at http://localhost:8080/\n";
