11824cb5fb52184e9df3f859b3e50c4df55a8647
console.log(cat == cute) <qwreey75@gmail.com>
2022-07-12 03:49:06 +0900

    og 모듈 추가

---
+++ build/buildHTML/og.lua
@@ -0,0 +1,19 @@
+local module = {};
+
+local ogformat = '	<meta property="og:%s" content="%s">';
+local concat = table.concat;
+local insert = table.insert;
+
+function module.ogbuild(content)
+    local strs = {};
+
+    for index,value in pairs(content) do
+        if value then
+            insert(strs,ogfotmat:format(index:gsub("_",":"),value));
+        end
+    end
+    
+    return concat(strs,"\n")
+end
+
+return module;
